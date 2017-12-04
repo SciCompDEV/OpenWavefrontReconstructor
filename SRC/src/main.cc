025721978f2939d2d57992a903fb44cd9d8a1374
Vazquez-Otero, Alejandro <alejandro.vazquez.otero@merck.com>
2017-12-04 10:54:25 +0100

    cleaning main.cc

--- SRC/src/main.cc
+++ SRC/src/main.cc
@@ -1,10 +1,8 @@
 // -----------------------------------------------------------------------------
 #include <string>
-#include <memory> // unique
+#include <memory> 
 #include <iomanip>
 #include <iostream>
-//#include <exception>
-//#include <stdexcept>
 #include <sstream>
 // -----------------------------------------------------------------------------
 #include "CLogging.h"
@@ -13,61 +11,48 @@
 #include "WaveFrontReconstructor.h"
 #include "MockWavefrontGenerator.h"
 #include "CFactory.h"
-#include "CParser.h"
 #include "CParsening.h"
 #include "AuxiliaryTemporaryFunctions.h"
 // -----------------------------------------------------------------------------
-using namespace std;
+using namespace std; // TODO
 CLogging MAIN((char*)"Log4cxxConfig.xml","MAIN");
 //
-//int main(int argc, char** argv) {
 int main() {
-    // TODO: CParsening parser;
-    // TODO: Decouple Mock from the contructor of WaveFrontReconstructor.
-    // The MockWavefrontGenerator is an independent class, which
-    // should be chosen by the user, and it may be replaced
-    // in runtime by the sensor's slopes retrieving function.
-    //bool generate_only=true;
     int ret =-1;
     try {
         CParsening parser("config.cfg"); // TODO: move class to static functions?
         
         // Building data -------------------------------------------------------
-        unique_ptr<DATA> input_data(new DATA(parser.get_mock_type_wavefront()));
-        //unique_ptr<DATA> input_data(new DATA(parser.get_mock_type_wavefront(), 30));
+        unique_ptr<DATA> input_data(new DATA(parser.get_mock_type_wavefront())); // TODO: confirm the 30 is not neccesary anymore
         // OR:
         //std::vector<double> x, y, z, dx, dy;
-        //unique_ptr<DATA> input_data(new DATA(x, y, z, dx, dz ));
+        //unique_ptr<DATA> input_data(new DATA(x, y, z, dx, dz )); // TODO: check if  thi sis correct:in runtime by the sensor's slopes retrieving function.
+        
         // Buiding reconstructor -----------------------------------------------
-        // MockWavefrontGenerator should return only the values in case DATA is a user input 
         auto reconstructor = CFactory::make(parser.get_pl_basis(), parser.get_pl_order(), new MockWavefrontGenerator(std::move(input_data)));
+
         // ---------------------------------------------------------------------
-        // Testing zone (used for checking numerical procedure, please
-        // do not erase until numerical algorithms are fixed.
         MAIN.INFO((char *)"Computing image reconstruction...");
         // TODO: compute instead of ComputeReconstructedWaveFront
         // TODO: why do you need parsing the wf twice? here and in the factory#make?
         vector<double> zReconstructed = reconstructor->ComputeReconstructedWaveFront(reconstructor->data->x,reconstructor->data->y,reconstructor->_coeffs);
-        cout << scientific << setprecision(10) << endl;
-        // TODO: encapsulate all this stuff
-        cout << "NumberOfNodes: " << reconstructor->data->dx.size() << endl;
-        cout << "CPUTimeSVD: " << reconstructor->GetCPUTimeMatrixGeneration() << endl;
-        cout << "CPUTimePureSVD: " << reconstructor->GetCPUTimePureSVD() << endl;
-        cout << "CPUTimeGenMatrixM: " << reconstructor->GetCPUTimeGenerationOfMatrixM() << endl;
-        cout << "CPUTimeGenMatrixR: " << reconstructor->GetCPUTimeGenerationOfMatrixR() << endl;
-        cout << "SingleCPUTimeVSUGProduct: " << reconstructor->GetCPUTimeCoefficientEstimation() << endl;
-        cout << "SingleCPUTimeRAProduct: " << reconstructor->GetCPUTimeSimpleReconstruction() << endl;
-        // ---------------------------------------------------------------------
-        // TODO: twice the same call??
+
+        reconstructor->print_CPU_gathered_info();
+
+        // TODO: Solano, i want to move the args to a return value, why are calling this function twice?
         reconstructor->CenterWavefrontAlongZ(reconstructor->data->z);
-        reconstructor->CenterWavefrontAlongZ(zReconstructed);
-        // ---------------------------------------------------------------------
-        AuxiliaryTemporaryFunctions::SaveData3D(reconstructor->data->x, reconstructor->data->y,reconstructor->data->z,string("generated.tsv"));
-        AuxiliaryTemporaryFunctions::SaveData3D(reconstructor->data->x,reconstructor->data->y,zReconstructed,string("reconstructed.tsv"));
-        AuxiliaryTemporaryFunctions::SaveComparedData3D(reconstructor->data->x, reconstructor->data->y,reconstructor->data->z,zReconstructed,string("difference.tsv"));
-        AuxiliaryTemporaryFunctions::SaveDiffData2D(reconstructor->data->x, reconstructor->data->y,reconstructor->data->z,zReconstructed,string("error.dat"));
+        reconstructor->CenterWavefrontAlongZ(zReconstructed); // TODO: Move this as a return values
+
+        AuxiliaryTemporaryFunctions::SaveData3D_gathered(reconstructor->data->x, 
+                reconstructor->data->y, 
+                reconstructor->data->z, 
+                zReconstructed, "generated.tsv", 
+                "reconstructed.tsv", 
+                "difference.tsv", 
+                "error.dat"); 
         // ---------------------------------------------------------------------
         MAIN.DEB((char *)"Finished Saving...");
+
         double sumtot,sumres;
         // TODO: the scientific has to be encapsulted in reconstructor + encapsulte + parse as an option
         cout << std::scientific << std::setprecision(12);
