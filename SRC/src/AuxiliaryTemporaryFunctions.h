a894dab0c5fb21ec151445e2e68cbd3496aaf956
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2017-12-12 21:58:03 -0600

    Partial commit, before changing ComputeReconstructedWaveFront

--- SRC/src/AuxiliaryTemporaryFunctions.h
+++ SRC/src/AuxiliaryTemporaryFunctions.h
@@ -15,10 +15,10 @@ public:
     static void MakePlot3DGnuplot(string gnuplotZLabel,string gnuplotTitle, string fdat=string("temp3d.tsv"),string fpdf=string("temp3d.pdf"));
     static void MakePlot2DGnuplot(string gnuplotYLabel,string gnuplotTitle, string fdat=string("temp2d.dat"),string fpdf=string("temp2d.pdf"));
     static void SaveComparedData3D(vector<double> &xx,vector<double> &yy,vector<double> &zor,vector<double> &zrec, string fileName=string("temp3dcomp.tsv"));
-    static void SaveDiffData2D(const vector<double> &xx,const vector<double> &yy,const vector<double> &zor,const vector<double> &zrec, string fileName=string("temp2d.dat"));
+    static void SaveDiffData2D(const vector<double> &zor,const vector<double> &zrec, string fileName=string("temp2d.dat"));
     // Final gather
     static void SaveData3D_gathered(vector<double>&, vector<double>&, vector<double>&, vector<double>&, string, string, string, string);
-    static void print_simu_param_gather(double, double, vector<double>&,vector<double>&, std::string, int, int);
+    static void print_simu_param_gather(vector<double>&,vector<double>&, std::string, int, int);
 };
 
 #endif
